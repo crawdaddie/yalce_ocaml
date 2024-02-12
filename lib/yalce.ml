79df0d5ff2b3e93ae69c6ba5b020af7011a87802
crawdaddie <adam.a.juraszek@googlemail.com>
2024-02-12 12:14:37 +0000

    ocaml experts hate this one weird hack:
    weirdly my lib & bin builds correctly, but utop cannot find any libs to
    link to
    for some reason providing a dummy c file
    with the include_dirs for the project set allows me to use the rest of
    the stubs defined in yalce.ml using ctypes

--- lib/yalce.ml
+++ lib/yalce.ml
@@ -1,2 +1,7 @@
-external start_audio: unit -> unit = "caml_start_audio"
-external midi_setup: unit -> unit = "caml_midi_setup"
+open Ctypes
+open Foreign
+
+let start_audio = foreign "setup_audio" (void @-> (returning void))
+let midi_setup = foreign "midi_setup" (void @-> (returning void))
+let version = foreign "_caml_get_version" (void @-> (returning int))
+
